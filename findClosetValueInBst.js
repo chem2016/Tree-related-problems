a83fd4db7e0256f066d9fabaad70cbdc1067592b
Haoyu Yu <yuhaoyuapp@gmail.com>
2019-04-29 18:33:22 -0400

    added modules and gitignore

---
+++ findClosetValueInBst.js
@@ -0,0 +1,32 @@
+function findClosestValueInBst(tree, target) {
+    // Write your code here.
+      console.log('Current value: ', tree.value)
+      console.log('Current left: ', tree.left.value)
+      console.log('Current right: ', tree.right.value)
+  
+      if(tree.value === target){
+          console.log('Exact target')
+          return tree.value
+      }
+      if(tree.value < target){
+          if(tree.right){
+              console.log('go right')
+              return findClosestValueInBst(tree.right, target)
+          }else{
+              return tree.value
+          }
+      }
+      else if(tree.value > target){
+          if(tree.left){
+              console.log('go left')
+              return findClosestValueInBst(tree.left, target)
+          }else{
+              return tree.value
+          }
+      }
+  }
+  
+  // Do not edit the line below.
+  
+  exports.findClosestValueInBst = findClosestValueInBst;
+  
---
+++ spec/findClosetValueInBst_spec.js
@@ -0,0 +1,9 @@
+const chai = require("chai")
+const findClosetValueInBst = require('../findClosetValueInBst')
+
+describe("findClosetValueInBst is a function", function() {
+    it("contains spec with an expectation", function() {
+      expect(true).toBe(true);
+    });
+  });
+
